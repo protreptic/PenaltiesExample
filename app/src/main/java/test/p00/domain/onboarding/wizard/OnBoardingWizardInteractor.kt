26e1070e460b8b2a634d537ea4d3f4b3bed9e678
Peter Bukhal <peter.bukhal@gmail.com>
2018-04-23 19:12:54 +0300

    Оптимизировал код

--- app/src/main/java/test/p00/domain/onboarding/wizard/OnBoardingWizardInteractor.kt
+++ app/src/main/java/test/p00/domain/onboarding/wizard/OnBoardingWizardInteractor.kt
@@ -12,9 +12,8 @@ class OnBoardingWizardInteractor(
         private val userRepository: UserRepository =
                                     UserRepositoryFactory.create()) {
 
-    fun tryAddDriver(rawName: String, rawNumber: String): Observable<Boolean> =
-            just(rawNumber)
-                .flatMap { validateDriver(it) }
+    fun tryAddDriver(rawName: String, number: String): Observable<Boolean> =
+                validateDriver(number)
                 .flatMap { valid -> when (valid) {
                     true -> userRepository
                         .fetch()
@@ -27,80 +26,81 @@ class OnBoardingWizardInteractor(
                                         drivers.clear()
                                         drivers.add(DriverLicense().apply {
                                             name = rawName
-                                            registrationNumber = rawNumber
+                                            registrationNumber = number
                                         })
                                     }) }
                         .flatMap { just(true) }
-                    else -> just(false)
-                } }
+                    else -> just(false) } }
 
     fun tryAddVehicle(rawName: String, rawNumber: String): Observable<Boolean> =
-            just(rawNumber)
-                .flatMap { validateVehicle(it) }
-                .flatMap { valid -> when (valid) {
-                    true -> userRepository
-                        .fetch()
-                        .flatMap { user -> userRepository.retain(
-                                    user.apply {
-                                        /*
-                                         * Добавляем только последний введенный
-                                         * пользователем номер.
-                                         */
-                                        vehicles.clear()
-                                        vehicles.add(Vehicle().apply {
-                                            name = rawName
-                                            number = rawNumber
-                                        })
-                                    }) }
-                        .flatMap { just(true) }
-                    else -> just(false)
-                } }
+                validateVehicle(rawNumber)
+                .flatMap { valid ->
+                    when (valid) {
+                        true -> userRepository
+                            .fetch()
+                            .flatMap { user -> userRepository.retain(
+                                        user.apply {
+                                            /*
+                                             * Добавляем только последний введенный
+                                             * пользователем номер.
+                                             */
+                                            vehicles.clear()
+                                            vehicles.add(Vehicle().apply {
+                                                name = rawName
+                                                number = rawNumber
+                                            })
+                                        }) }
+                            .flatMap { just(true) }
+                        else -> just(false) } }
 
     fun tryAddVehicleLicense(rawNumber: String): Observable<Boolean> =
-            just(rawNumber)
-                .flatMap { validateVehicleLicense(it) }
-                .flatMap { valid -> when (valid) {
-                    true -> userRepository
-                        .fetch()
-                        .flatMap { user -> userRepository.retain(
-                                    user.apply {
-                                        if (vehicles.isNotEmpty()) {
-                                            vehicles[0]?.registrationNumber = rawNumber
-                                        }
-                                    }) }
-                        .flatMap { just(true) }
-                    else -> just(true)
-                } }
+                validateVehicleLicense(rawNumber)
+                .flatMap { valid ->
+                    when (valid) {
+                        true -> userRepository
+                            .fetch()
+                            .flatMap { user -> userRepository.retain(
+                                        user.apply {
+                                            if (vehicles.isNotEmpty()) {
+                                                vehicles[0]?.registrationNumber = rawNumber
+                                            }
+                                        }) }
+                            .flatMap { just(true) }
+                        else -> just(true) } }
 
-    private val A1 = "[АВЕКМНОРСТУХABEKMHOPCTYX]"
-    private val B2 = "[0-9]"
+    companion object {
 
-    /**
-     * Регистрационные знаки частных транспортных средств. Введены в 1993 году.
-     * Сначала имели формат — буква — 3 цифры — 2 буквы, код региона, но отсутствовал флаг.
-     * Флаг на номерах появляется в 1994 году, хотя в некоторых регионах, например,
-     * в Татарстане, Башкортостане, Санкт-Петербурге и Приморье,
-     * номера 1993 года (иногда параллельно) выдавались до первой половины 2000-х гг.
-     * Формат знаков — 3 буквы, 3 цифры. Буквы означают серию номерного знака, а цифры — номер.
-     * В правой части номерного знака помещается код региона регистрации и
-     * флага России с надписью RUS (см. ниже таблицу кодов регионов).
-     *
-     * До 1 июля 2008 года регистрационный знак, выданный грузовому автомобилю или автобусу,
-     * должен был быть продублирован на задней поверхности ТС крупным шрифтом.
-     * С 1 июля 2008 года данное требование отменено[2][нет в источнике].
-     */
-    private val PATTERN_VEHICLE_NUMBER_AUTO = "$A1$B2{3}$A1{2}$B2{2,3}" //Б ЦЦЦ ББ ЦЦ(Ц)
+        private const val C1 = "[АВЕКМНОРСТУХABEKMHOPCTYX]"
+        private const val C2 = "[0-9]"
 
-    /**
-     * Регистрационные знаки, устанавливаемые на мотоциклы, мотороллеры, мопеды и мотонарты.
-     * Изготавливаются из квадратной светоотражающей пластины, с использованием цифр и букв
-     * чёрного цвета (4 цифры в верхней строке, две буквы в нижней).
-     * Номер региона регистрации — в нижнем правом углу, чуть выше — надпись RUS.
-     */
-    private val PATTERN_VEHICLE_NUMBER_MOTO = "$B2{4}$A1{2}$B2{2,3}" //ЦЦЦЦ ББ ЦЦ(Ц)
+        /**
+         * Регистрационные знаки частных транспортных средств. Введены в 1993 году.
+         * Сначала имели формат — буква — 3 цифры — 2 буквы, код региона, но отсутствовал флаг.
+         * Флаг на номерах появляется в 1994 году, хотя в некоторых регионах, например,
+         * в Татарстане, Башкортостане, Санкт-Петербурге и Приморье,
+         * номера 1993 года (иногда параллельно) выдавались до первой половины 2000-х гг.
+         * Формат знаков — 3 буквы, 3 цифры. Буквы означают серию номерного знака, а цифры — номер.
+         * В правой части номерного знака помещается код региона регистрации и
+         * флага России с надписью RUS (см. ниже таблицу кодов регионов).
+         *
+         * До 1 июля 2008 года регистрационный знак, выданный грузовому автомобилю или автобусу,
+         * должен был быть продублирован на задней поверхности ТС крупным шрифтом.
+         * С 1 июля 2008 года данное требование отменено[2][нет в источнике].
+         */
+        private const val PATTERN_VEHICLE_NUMBER_AUTO = "$C1$C2{3}$C1{2}$C2{2,3}" //Б ЦЦЦ ББ ЦЦ(Ц)
 
-    private val PATTERN_DRIVER_LICENSE_NUMBER = "$B2{2}($A1|$B2){2}$B2{6}" //ЦЦ ББ ЦЦЦЦЦЦ
-    private val PATTERN_VEHICLE_LICENSE_NUMBER = "$B2{2}($A1|$B2){2}$B2{6}" //ЦЦ ББ ЦЦЦЦЦЦ
+        /**
+         * Регистрационные знаки, устанавливаемые на мотоциклы, мотороллеры, мопеды и мотонарты.
+         * Изготавливаются из квадратной светоотражающей пластины, с использованием цифр и букв
+         * чёрного цвета (4 цифры в верхней строке, две буквы в нижней).
+         * Номер региона регистрации — в нижнем правом углу, чуть выше — надпись RUS.
+         */
+        private const val PATTERN_VEHICLE_NUMBER_MOTO = "$C2{4}$C1{2}$C2{2,3}" //ЦЦЦЦ ББ ЦЦ(Ц)
+
+        private const val PATTERN_DRIVER_LICENSE_NUMBER = "$C2{2}($C1|$C2){2}$C2{6}" //ЦЦ ББ ЦЦЦЦЦЦ
+        private const val PATTERN_VEHICLE_LICENSE_NUMBER = "$C2{2}($C1|$C2){2}$C2{6}" //ЦЦ ББ ЦЦЦЦЦЦ
+
+    }
 
     private fun prepareInput(string: String) =
             string.trim().toUpperCase().replace(" ", "")
