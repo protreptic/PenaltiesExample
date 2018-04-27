9744badd5f8e0850b50a28244d2b35261a204cb1
Peter Bukhal <peter.bukhal@gmail.com>
2018-04-27 22:11:56 +0300

    Добавил базовый класс диалога

---
+++ app/src/main/java/test/p00/presentation/activity/abs/AbsFragmentDialog.kt
@@ -0,0 +1,8 @@
+package test.p00.presentation.activity.abs
+
+import android.support.v4.app.DialogFragment
+
+/**
+ * Created by Peter Bukhal on 4/27/18.
+ */
+abstract class AbsFragmentDialog : DialogFragment()
